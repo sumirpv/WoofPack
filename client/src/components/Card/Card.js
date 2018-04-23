56f536b9ec98164c45eb11f99f3da55ec5cfb7dd
noble0423 <ryan.noble82@gmail.com>
2018-04-23 15:19:13 -0700

    commented out items on card.js

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -4,11 +4,11 @@ import "./Card.css";
 const Card = props => (
     <div 
         // The below info was borrowed from another App until we know all the info that we are putting into the card.
-        role="img"
-        aria-label="click item"
-        onClick={() => props.handleClick(props.id)}
-        style={{ backgroundImage: `url("${props.image}")` }}
-        className={`click-item${props.shake ? " shake" : ""}`}
+        // role="img"
+        // aria-label="click item"
+        // onClick={() => props.handleClick(props.id)}
+        // style={{ backgroundImage: `url("${props.image}")` }}
+        // className={`click-item${props.shake ? " shake" : ""}`}
     />
 );
 
