8beede07eda1c799685b358685d64f9b9d9d6c50
Fredrik Gustafson <Fredrik@macbooks-MacBook-Pro.local>
2018-04-23 20:57:38 -0700

    handle input change done

--- client/src/components/MyProfile/PersonalData/PersonalData.js
+++ client/src/components/MyProfile/PersonalData/PersonalData.js
@@ -1,47 +1,67 @@
 import React, { Component } from "react";
 
 
-const PersonalData = () => {
 
-    return (
-        <div className="row">
-        <div className="col s12">
-            <div className="card-panel grey lighten-5 z-depth-1">
-                <div className="row valign-wrapper">
-                    <form class="col s12">
-                        <div className="row">
-                            <div className="input-field col s6">
-                                <input id="first_name" type="text" className="validate" />
-                                <label for="first_name">First Name</label>
-                            </div>
-                            <div className="input-field col s6">
-                                <input id="last_name" type="text" className="validate" />
-                                <label for="last_name">Last Name</label>
-                            </div>
-                        </div>
-                        <div className="row">
-                            <div className="input-field col s6">
-                                <input id="Phone" type="text" className="validate" />
-                                <label for="Phone_number">Phone number</label>
-                            </div>
-                            <div className="input-field col s6">
-                                <input id="Address" type="text" className="validate" />
-                                <label for="Address">Address</label>
-                            </div>
-                        </div>
+class PersonalData extends Component {
+    state = {
+        firstName:"",
+        lastName: "",
+        phone: "",
+        address: "",
+        aboutMe: "",
 
-                        <div className="row">
-                            <div class="input-field col s12">
-                                <textarea id="about_me" className="materialize-textarea"></textarea>
-                                <label for="about_me">About Me</label>
+    };
+
+    handleInputChange = event => {
+        const { name, value } = event.target;
+        this.setState({
+          [name]: value
+        });
+      };
+
+
+
+    render() {      
+        return (
+            <div className="row">
+            <div className="col s12">
+                <div className="card-panel grey lighten-5 z-depth-1">
+                    <div className="row valign-wrapper">
+                        <form class="col s12">
+                            <div className="row">
+                                <div className="input-field col s6">
+                                    <input name="firstName" value={this.state.firstName} onChange={this.handleInputChange} id="firstName" type="text" className="validate" />
+                                    <label for="firstName">First Name</label>
+                                </div>
+                                <div className="input-field col s6">
+                                    <input name="lastName" value={this.state.lastName} onChange={this.handleInputChange} id="lastName" type="text" className="validate" />
+                                    <label for="lastName">Last Name</label>
+                                </div>
                             </div>
-                        </div>
-                    </form>
+                            <div className="row">
+                                <div className="input-field col s6">
+                                    <input name="phone"  value={this.state.phone} onChange={this.handleInputChange} id="phone" type="text" className="validate" />
+                                    <label for="phone">Phone number</label>
+                                </div>
+                                <div className="input-field col s6">
+                                    <input name="address"  value={this.state.address} onChange={this.handleInputChange} id="address" type="text" className="validate" />
+                                    <label for="address">Address</label>
+                                </div>
+                            </div>
+
+                            <div className="row">
+                                <div class="input-field col s12">
+                                    <textarea name="aboutMe"  value={this.state.aboutMe} onChange={this.handleInputChange} id="aboutMe" className="materialize-textarea"></textarea>
+                                    <label for="aboutMe">About Me</label>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    )
+        )
+    }
 }
 
 export default PersonalData;
