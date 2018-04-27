caaeacdc38756734822d4e289174b67c98b892d6
Bianca Torres <biancatorres@Biancas-MacBook.local>
2018-04-27 14:26:18 -0700

    my pack page works with multiple pack members

--- client/src/pages/MyPack/MyPack.js
+++ client/src/pages/MyPack/MyPack.js
@@ -8,7 +8,8 @@ class MyPack extends Component {
 
         this.state = {
             myPack: [],
-            myPackInfo: []
+            myPackInfo: [],
+            packArray: []
         }
     }
     //API get route from db to grab pack members, add to state and then map over them 
@@ -23,53 +24,39 @@ class MyPack extends Component {
         var usernameObj = { username: this.props.username };
         //Get the IDs and all data of those are in myPack property in the DB for this user
         API.getPack(usernameObj).then(res => {
-            console.log(res);
+            console.log(res.data);
             this.setState({
-                myPackInfo: [...this.state.myPackInfo, res.data]
+                myPackInfo: res.data,
+                packArray: res.data[0].myPack
             });
             
         }).catch(error => console.log(error));
     };
 
-    // getPackInfo = () => {
-    //     // console.log('getbackinfo function has been fired');
-    //     console.log(this.state.myPack)
-    //     for (let i = 0; i < this.state.myPack.length; i++) {
-    //         var usernameObj = {username: this.state.myPack[i]};
-    //         console.log(usernameObj);
-    //         API.getPackInfo(usernameObj).then(response => {
-    //             console.log(response);
-    //             this.setState({
-    //                 myPackInfo: [...this.state.myPackInfo, response.data]
-    //             })
-    //         });
-    //     };
-    // };
-
     render() {
-        console.log('state');
-    console.log(this.state.myPackInfo);
         return (
             <div>
                 <h1> {this.props.username}'s WoofPack </h1>
                 {console.log(this.state.myPackInfo)}
-                {this.state.myPackInfo.map(member => {
+                {this.state.packArray.map(member => {
                     return (
-                     <Card 
-                     address = {member[0].address}
-                     dog = {member[0].dog}
-                     firstname = {member[0].firstname}
-                     lastname = {member[0].lastname}
-                     phone = {member[0].phone}
-                     email = {member[0].email}
-                     picture = {member[0].picture}
-                     about = {member[0].about}
-                     address = {member[0].address}
-                     username = {member[0].user}
-                     _id = {member[0]._id}
-                     message = {member[0].message}
-                     />
-                    )
+                        <Card 
+                        address = {member.address}
+                        dog = {member.dog}
+                        firstname = {member.firstname}
+                        lastname = {member.lastname}
+                        phone = {member.phone}
+                        email = {member.email}
+                        picture = {member.picture}
+                        about = {member.about}
+                        address = {member.address}
+                        username = {member.user}
+                        _id = {member._id}
+                        message = {member.message}
+                        />
+                       )
+                    
+
                     
                 })}
                
