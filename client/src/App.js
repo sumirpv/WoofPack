db508c1579ef15cf9f9e21bf31a74c7e6f9b598d
noble0423 <ryan.noble82@gmail.com>
2018-04-26 23:03:25 -0700

    NavHeader is done.

--- client/src/App.js
+++ client/src/App.js
@@ -6,10 +6,10 @@ import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
 import FindPack from "./pages/FindPack";
 import MyPack from "./pages/MyPack";
-import Wrapper from "./components/Wrapper"
+import Wrapper from "./components/Wrapper";
+import NavHeader from "./components/NavHeader";
 import { BrowserRouter as Router, Route, Switch,  Redirect  } from "react-router-dom"
 import API from './utils/API.js'
-
 import "./App.css";
 
 class App extends Component {
@@ -43,6 +43,7 @@ class App extends Component {
     return (
       <Router>
         <div>
+          <NavHeader />
           <Nav firstname={this.state.firstname}
           email={this.state.email}
           picture= {this.state.picture}/>
@@ -53,6 +54,7 @@ class App extends Component {
               <Route exact path="/findpack" 
                 render={() => (this.state.session === true ? <FindPack/> : <Home/>)}/> 
               <Route exact path="/mypack" render={() => (this.state.session === true ? <MyPack username={this.state.username} /> : <Home/>)}/>
+              {/* <Route exact path="/logout" component={Logout} /> */}
             </Switch>
           </Wrapper>
         </div>
