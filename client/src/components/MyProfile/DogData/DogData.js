44f20df3bd248cf588133ee855fa580e9df2a080
Fredrik Gustafson <Fredrik@macbooks-MacBook-Pro.local>
2018-04-30 13:41:12 -0700

    fixed alignement on dog profile

--- client/src/components/MyProfile/DogData/DogData.js
+++ client/src/components/MyProfile/DogData/DogData.js
@@ -1,51 +1,32 @@
-import React, { Component } from "react";
+import React from "react";
+import "./DogData.css";
 
 
 const DogData = (props) => {
 
     return (
-        <div className="row">
-            <div className="col s12">
-                <div className="card-panel grey lighten-5 z-depth-1">
-                    <div className="row valign-wrapper">
-                        <div className="col s2">
-                            <img src={props.picture} alt="" class="circle responsive-img" />
-                        </div>
-                        <div className="col s5">
+        <div className="card-panel grey lighten-5 z-depth-1">
+            <div className="row">
+                    <div className="col s2">
+                        <img src={props.picture} alt="" class="circle responsive-img" />
+                    </div>
+                    <div className="col s5">
                         <div> <span> <h5>{props.name} </h5></span> </div>
-                            <span className="black-text">
-
-                                <strong> Breed:   </strong>
-                                {props.breed}
-                            </span><br/>
-                            <span className="black-text">
-                                <strong> Size:   </strong>
-                                {props.size}
-                            </span><br/>
-
-                        
-                       
-                            <span className="black-text">
-                                <strong> Age:   </strong>
-                                {props.age}
-                            </span><br/>
-                            <span className="black-text">
-                                <strong> Temperment:   </strong>
-                                {props.temperment}
-                            </span><br/>
-
-                            </div>
-                    </div>
-                    <div className="row valign-wrapper">
-                    <div className="col s8">
-                    <span className="black-text">
-                                <strong> About Me:   </strong>
-                                {props.about}
-                            </span><br/>
-                    </div>
-
-                    </div>
-                </div>
+                        <span className="black-text">
+                            <strong> Breed:   </strong>{props.breed}</span><br/>
+                        <span className="black-text">            
+                            <strong> Size:   </strong> {props.size} </span><br/>      
+                        <span className="black-text">        
+                            <strong> Age:   </strong> {props.age}  </span><br/>          
+                    </div>        
+                                
+                    <div className="col s5">
+                    <span className="black-text">           
+                            <strong> Temperment:   </strong> {props.temperment}</span><br/>
+                    <span id="aboutDog" className="black-text">
+                        <strong> About Me:   </strong>{props.about} efjhbwehcbehrbcveh veh cehrbc erhcv rhveverv evueuv ueerv </span>
+                    </div>            
+                           
             </div>
         </div>
 
