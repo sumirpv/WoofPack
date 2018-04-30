d2fa416eb104d6628e924bb15339da6b0631f381
Bianca Torres <biancatorres@Biancas-MacBook.local>
2018-04-30 10:41:01 -0700

    sign up back

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-// import Modal from "../../components/SignupModal";
+import Modal from "../../components/SignupModal";
 import Login from "../../components/Login"
 import "./Home.css";
-import { Slider, Slide, Parallax, Button, Modal } from "react-materialize";
+import { Slider, Slide, Parallax, Button,  } from "react-materialize";
 
 // import Row from "../../Grid/row.js"
 
@@ -66,27 +66,26 @@ class Home extends Component {
                     {this.props.loggedIn === true ? null :  <button onClick={this.clickSignupModal} > Sign up </button> }
                     {this.props.loggedIn === true ? null :  <button onClick={this.clickLoginModal} > Login </button> }
                     
-                  
 
-                
                 {this.openSignupModal()}
                 {this.openLoginModal()}
-            {/* <Slider className="slider-home" fullscreen="true" interval={10000}> 
+            <Slider className="slider-home" fullscreen="true" interval={10000}> 
                 <Slide
                     src="https://www.protectivity.com/protectivity-blog/wp-content/uploads/sites/102/2015/09/dog-walking.jpg"
                     title="This is our big Tagline!">
                     Here's our small slogan.
                     <button onClick={this.clickSignupModal} > Sign up </button>
                     <button onClick={this.clickLoginModal} > Login </button>
-                     <Modal  
+                     {/* <Modal  
                         header='Modal Header'
                         fixedFooter
                         trigger={<Button>MODAL WITH FIXED FOOTER</Button>}>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-                    </Modal>
+                    </Modal> */}
                     {this.openSignupModal()}
                     {this.openLoginModal()} 
                 </Slide>
+
                 <Slide
                     src="https://dingo.care2.com/pictures/greenliving/1404/1403349.large.jpg"
                     title="Left aligned Caption"
@@ -121,18 +120,18 @@ class Home extends Component {
                 
                 
             </Slider> 
-            {/* <div>
-                <Parallax imageSrc="https://www.professionalpetnanny.com/wp-content/themes/petnanny/images/cincinnati-dog-walking-slide.jpg"/>
+            <div>
+                {/* <Parallax imageSrc="https://www.professionalpetnanny.com/wp-content/themes/petnanny/images/cincinnati-dog-walking-slide.jpg"/>
                     <div className="section white">
                         <div className="row container">
                             <h2 className="header">Parallax</h2>
                             <p className="grey-text text-darken-3 lighten-3">Parallax is an effect where the background content or image in this case, is moved at a different speed than the foreground content while scrolling.</p>
                         </div>
                     </div>
-                <Parallax imageSrc="https://www.protectivity.com/protectivity-blog/wp-content/uploads/sites/102/2015/09/dog-walking.jpg"/>
+                <Parallax imageSrc="https://www.protectivity.com/protectivity-blog/wp-content/uploads/sites/102/2015/09/dog-walking.jpg"/> */}
             </div>
             {this.openSignupModal()}
-            {this.openLoginModal()} */}
+            {this.openLoginModal()}
 
             </div>
         )
